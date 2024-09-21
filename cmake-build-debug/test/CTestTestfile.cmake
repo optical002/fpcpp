de2452fb1d8649652ba6db5f812a090bca60bae9
OpTick <pauliussuku@gmail.com>
2024-09-22 00:25:26 +0300

    removed tracking reference

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/work/fpcpp/test
-# Build directory: C:/work/fpcpp/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[AllTestsInMain]=] "core_tests")
-set_tests_properties([=[AllTestsInMain]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/work/fpcpp/test/CMakeLists.txt;42;add_test;C:/work/fpcpp/test/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
